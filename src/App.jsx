07232636999ed49f2efb32d420cc05242b62e8a2
Gaurav Upadhyay <upadhyaykumar2003@gmail.com>
2025-04-02 13:22:05 +0530

    Added navbar and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import "./App.css";
 import ApplyForm from "./pages/ApplyForm";
 import InternshipDetails from "./pages/InternshipDetails";
 import InternshipTraining from "./pages/InternshipTraining";
+import Footer from "./components/Footer";
+import Navbar from "./pages/Navbar";
+
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <InternshipTraining />
-      <InternshipDetails />
-      <ApplyForm />
+      {/* <InternshipDetails /> */}
+      {/* <ApplyForm /> */}
+      <Footer/> 
     </div>
   );
 }
